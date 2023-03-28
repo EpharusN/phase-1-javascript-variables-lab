f75db811d3744cf5a704e0586484d42801f37e60
Epharus Ndumia <epharusndumia@gmail.com>
2023-03-29 00:01:58 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file!
+//npmlet companyName = 'Scuber';
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+//const companyCeo = 'Susan Smith';
+let companyCeo = 'Susan Smith';
